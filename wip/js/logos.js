12e097bf56971d2bb18c56693487f6c28d0419bc
Francisco Esteves <xicodax@gmail.com>
2023-04-17 02:30:37 +0100

    Alterações para a página das conclusões III

--- wip/js/logos.js
+++ wip/js/logos.js
@@ -5,92 +5,92 @@ var Logos = new Vue({
             {
                 "alt": "Com o Alto Patrocínio de Sua Excelência o Presidente da Républica",
                 "style": "width:100%;padding:5px;",
-                "src": "img/PAT00.jpg"
+                "src": "../img/PAT00.jpg"
             },
             {
                 "alt": "Logotipo Ministério Público",
                 "style": "height:80px; padding:5px;",
-                "src": "img/CCCO01.jpg"
+                "src": "../img/CCCO01.jpg"
             },
             {
                 "alt": "Logotipo Provedor de Justiça",
                 "style": "height:80px;padding:5px;",
-                "src": "img/PJUS.jpg"
+                "src": "../img/PJUS.jpg"
             },
             {
                 "alt": "Logotipo Ordem dos Advogados",
                 "style": "height:100px; padding:5px;",
-                "src": "img/CCCO04.jpg"
+                "src": "../img/CCCO04.jpg"
             },
             {
                 "alt": "Logotipo CNPDPCJ",
                 "style": "height:100px; padding:5px;",
-                "src": "img/CCCO03.jpg"
+                "src": "../img/CCCO03.jpg"
             },
             {
                 "alt": "Logotipo DGRSP",
                 "style": "height:80px; padding:5px;",
-                "src": "img/CCCO02.jpg"
+                "src": "../img/CCCO02.jpg"
             },
             {
                 "alt": "Logotipo CEJ",
                 "style": "height:80px; padding:5px;",
-                "src": "img/CCCO08.jpg"
+                "src": "../img/CCCO08.jpg"
             },
             {
                 "alt": "Logotipo Segurança Social",
                 "style": "padding:5px;",
-                "src": "img/CCCO06.jpg"
+                "src": "../img/CCCO06.jpg"
             },
             {
                 "alt": "Logotipo SCML",
                 "style": "height:100px; padding:5px;",
-                "src": "img/16.jpg"
+                "src": "../img/16.jpg"
             },
             {
                 "alt": "Logotipo Casa Pia",
                 "style": "height:100px; padding:5px;",
-                "src": "img/CP.jpg"
+                "src": "../img/CP.jpg"
             },
             {
                 "alt": "Logotipo CDFUC",
                 "style": "height:80px; padding:5px;",
-                "src": "img/CCCO07.jpg"
+                "src": "../img/CCCO07.jpg"
             },
             {
                 "alt": "Logotipo NOVA FCSH",
                 "style": "height:100px; padding:5px;",
-                "src": "img/FCSH.jpg"
+                "src": "../img/FCSH.jpg"
             },
             {
                 "alt": "Logotipo CICS",
                 "style": "height:100px; padding:5px;",
-                "src": "img/CICS.jpg"
+                "src": "../img/CICS.jpg"
             },
             {
                 "alt": "Logotipo FCT",
                 "style": "height:80px; padding:5px;",
-                "src": "img/CCCO14.jpg"
+                "src": "../img/CCCO14.jpg"
             },
             {
                 "alt": "Logotipo Universidade Atlântica",
                 "style": "height:80px; padding:5px;",
-                "src": "img/CCCO09.jpg"
+                "src": "../img/CCCO09.jpg"
             },
             // {
             //     "alt": "Logotipo Cáritas",
             //     "style": "height:80px; padding:5px;",
-            //     "src": "img/CCCO10.jpg"
+            //     "src": "../img/CCCO10.jpg"
             // },
             {
                 "alt": "Logotipo CrescerSer",
                 "style": "height:80px; padding:5px;",
-                "src": "img/CCCO12.jpg"
+                "src": "../img/CCCO12.jpg"
             },
             {
                 "alt": "Logotipo ComDignitatis",
                 "style": "height:80px; padding:5px;",
-                "src": "img/CCCO13.jpg"
+                "src": "../img/CCCO13.jpg"
             }
         ]
     }
@@ -103,52 +103,52 @@ var Patrocinios = new Vue({
             {
                 "alt": "Logotipo Glintt",
                 "style": "height:100px; padding:5px;",
-                "src": "img/PAT04.jpg"
+                "src": "../img/PAT04.jpg"
             },
             {
                 "alt": "Logotipo Crédito Agrícola Mútuo",
                 "style": "height:100px; padding:5px;",
-                "src": "img/PAT02a.jpg"
+                "src": "../img/PAT02a.jpg"
             },
             {
                 "alt": "Logotipo Centro Internacional de Ensino e Investigação Fernão de Magalhães",
                 "style": "width:100px;padding:5px;",
-                "src": "img/PAT07.jpg"
+                "src": "../img/PAT07.jpg"
             },
             {
                 "alt": "Logotipo Grupo Lourenço & Lousada",
                 "style": "height:100px; padding:5px;",
-                "src": "img/PAT05.jpg"
+                "src": "../img/PAT05.jpg"
             },
             {
                 "alt": "Logotipo Martins Alves",
                 "style": "height:50px; padding:5px;",
-                "src": "img/PAT03.jpg"
+                "src": "../img/PAT03.jpg"
             },
             {
                 "alt": "Logotipo Hotel A.S.",
                 "style": "height:100px; padding:5px;",
-                "src": "img/PAT09.jpg"
+                "src": "../img/PAT09.jpg"
             },
             {
                 "alt": "Logotipo Cerger",
                 "style": "height:80px;padding:5px;",
-                "src": "img/PAT10.jpg"
+                "src": "../img/PAT10.jpg"
             },
             {
                 "alt": "Logotipo A Casa das Casas",
                 "style": "height:150px;padding:5px;",
-                "src": "img/PAT11.jpg"
+                "src": "../img/PAT11.jpg"
             },
             {
                 "alt": "Logotipo Grupo das Casas",
                 "style": "height:150px;padding:5px;",
-                "src": "img/PAT12.jpg"
+                "src": "../img/PAT12.jpg"
             },
             {
                 "alt": "Logotipo",
                 "style": "height:150px;padding:5px;",
-                "src": "img/PAT13.jpg"
+                "src": "../img/PAT13.jpg"
             }
         ]
     }
@@ -159,14 +159,19 @@ var Parceiros = new Vue({
     "data": {
         "images": [
             {
-                "alt": "Logotipo Ordem Psicólogos",
+                "alt": "Logotipo Camara Municipal Oleiros",
                 "style": "height:100px; padding:5px;",
-                "src": "img/PAR06.jpg"
+                "src": "img/23_05-PAR-001.jpg"
             },
             {
                 "alt": "Logotipo do Centro de Formação da Associação de Escolas Rómulo de Carvalho",
                 "style": "height:100px; padding:5px;",
-                "src": "img/PAR01.jpg"
+                "src": "img/23_05-PAR-002.png"
+            },
+            {
+                "alt": "Logotipo Ordem dos Psicologos",
+                "style": "height:100px; padding:5px;",
+                "src": "img/23_05-PAR-003.jpg"
             }
         ]
     }
@@ -179,12 +184,12 @@ var Apoios = new Vue({
             {
                 "alt": "Logotipo Justiça TA",
                 "style": "height:100px; padding:5px;",
-                "src": "img/AP01.jpg"
+                "src": "../img/AP01.jpg"
             },
             {
                 "alt": "Logotipo DATAJURIS",
                 "style": "height:100px; padding:5px;",
-                "src": "img/AP02.jpg"
+                "src": "../img/AP02.jpg"
             }
         ]
     }
