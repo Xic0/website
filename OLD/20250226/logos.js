845d419c25ea3729517712729c7c9c7d1daeaaff
Francisco Esteves <xicodax@gmail.com>
2025-02-26 23:28:11 +0000

    backup

---
+++ OLD/20250226/logos.js
@@ -0,0 +1,154 @@
+var Logos = new Vue({
+    "el": "#idLogos",
+    "data": {
+        "images": [
+            {
+                "alt": "Com o Alto Patrocínio de Sua Excelência o Presidente da Républica",
+                "style": "width:100%;padding:5px;",
+                "src": "img/PAT00_2.png"
+            },
+            {
+                "alt": "Logotipo Ministério Público",
+                "style": "height:80px; padding:5px;",
+                "src": "/img/CCCO01.jpg"
+            },
+            {
+                "alt": "Logotipo Provedor de Justiça",
+                "style": "height:80px;padding:5px;",
+                "src": "/img/PJUS.jpg"
+            },
+            {
+                "alt": "Logotipo Ordem dos Advogados",
+                "style": "height:100px; padding:5px;",
+                "src": "/img/CCCO04.jpg"
+            },
+            {
+                "alt": "Logotipo CNPDPCJ",
+                "style": "height:100px; padding:5px;",
+                "src": "/img/CCCO03.jpg"
+            },
+            {
+                "alt": "Logotipo República Portuguesa Justiça",
+                "style": "height:100px; padding:5px;",
+                "src": "/img/PAT14.png"
+            },
+            {
+                "alt": "Logotipo DGRSP",
+                "style": "height:100px; padding:5px;",
+                "src": "/img/CCCO02a.jpg"
+            },
+            {
+                "alt": "Logotipo CEJ",
+                "style": "height:80px; padding:5px;",
+                "src": "/img/CCCO08.jpg"
+            },
+            // {
+            //     "alt": "Logotipo Segurança Social",
+            //     "style": "padding:5px;",
+            //     "src": "/img/CCCO06.jpg"
+            // },
+            {
+                "alt": "Logotipo SCML",
+                "style": "height:100px; padding:5px;",
+                "src": "/img/16.jpg"
+            },
+            {
+                "alt": "Logotipo Casa Pia",
+                "style": "height:100px; padding:5px;",
+                "src": "/img/CP.jpg"
+            },
+            {
+                "alt": "Logotipo CDFUC",
+                "style": "height:80px; padding:5px;",
+                "src": "/img/CCCO07.jpg"
+            },
+            {
+                "alt": "Logotipo NOVA FCSH",
+                "style": "height:100px; padding:5px;",
+                "src": "/img/FCSH.jpg"
+            },
+            {
+                "alt": "Logotipo CICS",
+                "style": "height:130px; padding:5px;",
+                "src": "/img/CICS.jpg"
+            },
+            {
+                "alt": "Logotipo FCT",
+                "style": "height:100px; padding:5px;",
+                "src": "/img/CCCO15a.jpg"
+            },
+            {
+                "alt": "Logotipo Universidade Atlântica",
+                "style": "height:80px; padding:5px;",
+                "src": "/img/CCCO09.jpg"
+            },
+            {
+                "alt": "Logotipo CrescerSer",
+                "style": "height:80px; padding:5px;",
+                "src": "/img/CCCO12.jpg"
+            },
+            {
+                "alt": "Logotipo ComDignitatis",
+                "style": "height:80px; padding:5px;",
+                "src": "/img/CCCO13.jpg"
+            }
+        ]
+    }
+})
+
+var Parceiros = new Vue({
+    "el": "#idParceiros",
+    "data": {
+        "images": [
+            { "alt": "Logotipo Agrupamento de Escolas de Latino Coelho", "style": "height:100px; padding:5px;", "src": "img/PAR24.png" },
+            { "alt": "Logotipo Agrupamento de Escolas Queluz-Belas", "style": "height:100px; padding:5px;", "src": "img/PAR12.png" },
+            { "alt": "Logotipo Centro de Formação da Associação de Escolas Rómulo de Carvalho", "style": "height:100px; padding:5px;", "src": "img/23_05-PAR-002.png" },
+            { "alt": "Logotipo Freguesia Queluz-Belas", "style": "height:100px; padding:5px;", "src": "img/PAR10.jpg" },
+            { "alt": "Logotipo Lamego", "style": "height:100px; padding:5px;", "src": "img/PAR11.png" },
+            { "alt": "Logotipo Ordem dos Psicólogos - Acção Formativa Acreditada", "style": "height:100px; padding:5px;", "src": "img/23_05-PAR-003.jpg" },
+        ]
+    }
+})
+
+var Patrocinios = new Vue({
+    "el": "#idPatrocinios",
+    "data": {
+        "images": [
+            { "alt": "Logotipo Cafilesa", "style": "height:100px; padding:5px;", "src": "img/2024_PAT01.jpg" },
+            { "alt": "Logotipo Caixa de Crédito Agrícula", "style": "height:130px; padding:5px;", "src": "img/2024_PAT02.jpg" },
+            { "alt": "Logotipo Comboios de Portugal", "style": "height:100px; padding:5px;", "src": "img/2024_PAT03.png" },
+            { "alt": "Logotipo Fundação Calouste Gulbenkian", "style": "height:80px; padding:5px;", "src": "img/2024_PAT04.jpg" },
+            { "alt": "Logotipo Gráfica Sobreirense", "style": "height:100px; padding:5px;", "src": "img/2024_PAT10.jpg" },
+            { "alt": "Logotipo Grupo Das Casas", "style": "height:130px; padding:5px;", "src": "img/2024_PAT05.jpg" },
+            { "alt": "Logotipo Casa das Casas", "style": "height:130px; padding:5px;", "src": "img/2024_PAT06.jpg" },
+            { "alt": "Logotipo Martins Alves", "style": "height:100px; padding:5px;", "src": "img/2024_PAT07.jpg" },
+            { "alt": "Logotipo Montepio", "style": "height:100px; padding:5px;", "src": "img/2024_PAT08.png" },
+            { "alt": "Logotipo Pestana", "style": "height:130px; padding:5px;", "src": "img/2024_PAT09.png" },
+        ]
+    }
+})
+
+// var Apoios = new Vue({
+//     "el": "#idApoios",
+//     "data": {
+//         "images": [
+//             // {
+//             //     "alt": "Logotipo Camara Municipal Oleiros",
+//             //     "style": "height:100px; padding:5px;",
+//             //     "src": "img/23_05-PAR-001.jpg"
+//             // },
+//             {
+//                 "alt": "Logotipo da Associação Mutualista Montepio Geral",
+//                 "style": "height:100px; padding:5px;",
+//                 "src": "img/2024_AP1.png"
+//             }
+//             // ,
+//             // {
+//             //     "alt": "Logotipo Ordem dos Psicologos",
+//             //     "style": "height:100px; padding:5px;",
+//             //     "src": "img/23_05-PAR-003.jpg"
+//             // }
+//         ]
+//     }
+// })
+
